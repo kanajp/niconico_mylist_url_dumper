1bc4c233cc0df9299d02fb1afece9266914ec811
Kazuki Nakajima <0xff.0xff.510@gmail.com>
2017-06-04 22:13:15 +0900

    ページが読み込まれるまえに、スクリプトが実行してしまわないように変更

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-$(function(){
-    var movie_url = [];
+$(function() {
+    setTimeout(function() {
+        var movie_url = [];
 
-    // URLを取得
-    $('h5').each(function() {
-        movie_url.push(('http://www.nicovideo.jp' + $(this).children('a').attr('href')));
-    });
+        // URLを取得
+        $('h5').each(function() {
+            movie_url.push(('http://www.nicovideo.jp' + $(this).children('a').attr('href')));
+        });
 
-    console.log(movie_url);
+        var dump = '';
+        for(var i = 0; i < movie_url.length; i++) {
+            dump += movie_url[i] + '\n';
+        }
 
-    var dump = '';
-    for(var i = 0; i < movie_url.length; i++) {
-        dump += movie_url[i] + '\n';
-    }
-
-    console.log(dump);
+        console.log(dump);
+    }, 2000);
 });
