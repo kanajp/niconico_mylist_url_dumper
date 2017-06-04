d64e5788b3ae8883efbdfe2b28c700f8bdfe1c83
Kazuki Nakajima <0xff.0xff.510@gmail.com>
2017-06-04 19:53:43 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(function(){
-    $('h5').each(function(i) {
-        console.log('http://www.nicovideo.jp' + $(this).children('a').attr('href'));
+    var prefix = '>play '
+    var movie_url = [];
+
+    // URLを取得
+    $('h5').each(function() {
+        movie_url.push(('http://www.nicovideo.jp' + $(this).children('a').attr('href')));
     });
+
+    var dump = '';
+    for(var i = 0; i < movie_url.length; i++) {
+        dump += prefix + movie_url[i] + '\n';
+    }
+
+    console.log(dump);
 });
