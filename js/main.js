1ff419bb6a80ac30c3dafc39ff67edf59645f764
Kazuki Nakajima <0xff.0xff.510@gmail.com>
2017-06-05 16:33:26 +0900

    無駄な処理を削除

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 $(function() {
     setTimeout(function() {
-        var movie_url = [];
+        var dump = '';
 
         // URLを取得
         $('h5').each(function() {
-            movie_url.push(('http://www.nicovideo.jp' + $(this).children('a').attr('href')));
+            dump += 'http://www.nicovideo.jp' + $(this).children('a').attr('href') + '\n';
         });
 
-        var dump = '';
-        for(var i = 0; i < movie_url.length; i++) {
-            dump += movie_url[i] + '\n';
-        }
-
-        console.log(dump);
+        console.log('\n\n%s', dump);
     }, 2000);
 });
