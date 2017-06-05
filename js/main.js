f4a555e0098aeb93fab015ad32ca83685bf872d2
Kazuki Nakajima <0xff.0xff.510@gmail.com>
2017-06-05 18:45:16 +0900

    他人のマイリストにも対応

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 $(function() {
     setTimeout(function() {
-        var dump = '';
+        var my = '';
+        var other = '';
 
         // URLを取得
         $('h5').each(function() {
-            dump += 'http://www.nicovideo.jp' + $(this).children('a').attr('href') + '\n';
+            my += 'http://www.nicovideo.jp' + $(this).children('a').attr('href') + '\n';
+        });
+        $('.font16').each(function() {
+            other += 'http://www.nicovideo.jp' + $(this).children('a').attr('href') + '\n';
         });
 
-        console.log('\n\n%s', dump);
+        if(my != '') {
+            console.log('\n\n%s', my);
+        }
+        if(other != '') {
+            console.log('\n\n%s', other);
+        }
     }, 2000);
 });
